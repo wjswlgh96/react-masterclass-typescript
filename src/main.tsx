74729e0c2859a96f86132740c9da2e4a78a26959
HoGirrr <wjswlgh96@naver.com>
2026-03-23 17:09:54 +0900

    Day02: typescript 완료

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { ThemeProvider } from "styled-components";
 import App from "./App.tsx";
+import { darkTheme } from "./theme.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 );
---
+++ src/theme.ts
@@ -0,0 +1,13 @@
+import type { DefaultTheme } from "styled-components";
+
+export const lightTheme: DefaultTheme = {
+  backgroundColor: "whitesmoke",
+  textColor: "#111",
+  buttonColor: "tomato",
+};
+
+export const darkTheme: DefaultTheme = {
+  backgroundColor: "#111",
+  textColor: "whitesmoke",
+  buttonColor: "teal",
+};
